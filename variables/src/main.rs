94d9235718b16efdddd6d6d6cd8f6efa589e4072
ggggght <wz9712203617@gmail.com>
2021-12-17 14:47:06 +0800

    feat: add copyright

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 // const MAX_POINTS: u32 = 100_000;
 fn main() {
     // let tup = (500, 6.4, 1);
